5ac0c79df18cd54fe68fa4fa958917433341ced9
nonspace <32026087+n0nspace@users.noreply.github.com>
2022-10-28 23:51:55 +0200

    Themes page

--- js/nonspace-darkstar.js
+++ js/nonspace-darkstar.js
@@ -1,21 +1,4 @@
-$(document).ready(function() {
-  if ($(window).width() < 1101) {
-     $('#mobile_menu').load('/mobile_menu.html');
-     $('body').append(
-       '<div id="nnspc" style="position: fixed; top: 0; left: 0; padding: 13px; background: #181818; z-index: 9"><a style="font-family: helvetica; font-weight: bold; letter-spacing: -1px; color: #fff;" href="/">nonspace.</a></div>'
-     );
-  }
-
-  
-  /*NOT FOUND PAGE*/
-  if ($('p:contains("The URL you requested could not be found.")').html()) {
-    $('body').load('https://www.dl.dropboxusercontent.com/s/16uf3jiqa1zt5uk/error.html');
-  }
-  
-  /*TAG INFO ON THEME PAGE*/
-  if ((window.location.pathname.split("/")[2] == "nnspc+codes") || (window.location.pathname == "/tagged/nnspc%20codes")) {
-      $('.tag_info h1').replaceWith('<h1>viewing nonspace themes</h1>');
-    }
+document.addEventListener('DOMContentLoaded', function () {
   
     const toggleSwitch = document.querySelector('input[type="checkbox"]#mode_input');
     const toggleSwitchFun = document.querySelector('input[type="checkbox"]#mode_fun');
@@ -53,4 +36,4 @@ $(document).ready(function() {
     toggleSwitchFun.addEventListener('change', switchTheme, false);
     toggleSwitch.addEventListener('change', switchTheme, false);
   
-  });  
+  }, false);
